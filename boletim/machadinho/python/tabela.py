1969ad1a0956842972486b77a0cf73e95815817c
ggrl <gabrielgoes@usp.br>
2023-08-29 09:18:10 -0300

    tabela centralizada, tiny, porém com separadores de decimais e milhares para ajustar

--- boletim/machadinho/python/tabela.py
+++ boletim/machadinho/python/tabela.py
@@ -5,7 +5,7 @@ import csv
 input_csv = "../csv/events-2023-06-01-IT.csv"
 
 # Nome do arquivo de saída LaTeX
-output_tex = "Tabela.tex"
+output_tex = "/home/ipt/documents/ggrl/tarefa_latex/boletim/machadinho/tex/Tabela.tex"
 
 # Abre o arquivo CSV para leitura
 with open(input_csv, "r") as csv_file:
@@ -15,22 +15,21 @@ with open(input_csv, "r") as csv_file:
 
 # Abre o arquivo LaTeX para escrita
 with open(output_tex, "w") as tex_file:
-    tex_file.write("\\begin{center}\n")
-    tex_file.write("\\begin{table}[htbp]\n")
-    tex_file.write("    \\caption{Dados de Terremotos}\n")
-    tex_file.write("    \\label{tab:dados_terremoto}\n")
-    tex_file.write("    \\renewcommand{\\arraystretch}{1.5} % Ajusta espaçamento entre linhas da tabela\n")
-    tex_file.write("    \\small\n")
-    tex_file.write("    \\begin{tabular}{ccccccccc} % Defina o número de colunas de acordo com seu arquivo CSV\n")
-    tex_file.write("        \\toprule\n")
+    tex_file.write("\\begin{table}[htb!]\n")
+    tex_file.write("    \\begin{center} \n")
+    tex_file.write("        \\caption{Dados de Terremotos}\n")
+    tex_file.write("        \\renewcommand{\\arraystretch}{1.5} \n")
+    tex_file.write("        \\tiny\n")
+    tex_file.write("        \\begin{tabular}{ccccS[table-format=6.0]S[table-format=7.0]S[table-format=1.1]S[table-format=5.3]c} \n")
+    tex_file.write("            \\toprule\n")
 
     # Escreve o cabeçalho da tabela
-    tex_file.write("        " + " & ".join(data[0]) + " \\\\\n")
-    tex_file.write("        \\midrule\n")
+    tex_file.write("            " + " & ".join(data[0]) + " \\\\\n")
+    tex_file.write("            \\midrule\n")
 
     # Escreve o subcabeçalho da tabela
-    tex_file.write("        " + " & ".join(data[1]) + " \\\\\n")
-    tex_file.write("        \\midrule\n")
+    tex_file.write("            " + " & ".join(data[1]) + " \\\\\n")
+    tex_file.write("            \\midrule\n")
 
     # Escreve os dados da tabela
     for row in data[2:]:
@@ -40,12 +39,12 @@ with open(output_tex, "w") as tex_file:
                 formatted_row.append(str(int(float(col))))
             else:
                 formatted_row.append(col)
-        tex_file.write("        " + " & ".join(formatted_row) + " \\\\\n")
+        tex_file.write("            " + " & ".join(formatted_row) + " \\\\\n")
 
-    tex_file.write("        \\bottomrule\n")
-    tex_file.write("    \\end{tabular}\n")
-    tex_file.write("    \\caption*{Fonte:IPT.}")
+    tex_file.write("            \\bottomrule\n")
+    tex_file.write("        \\end{tabular}\n")
+    tex_file.write("        \\label{tab:dados_terremoto}\n")
+    tex_file.write("        \\caption*{Fonte:IPT.}\n")
+    tex_file.write("    \\end{center}\n")
     tex_file.write("\\end{table}\n")
-    tex_file.write("\\end{center}\n")
-    tex_file.write("\\newpage\n")
 
