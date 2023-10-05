b5de9a9da45c4cadbc68dfef9cb79b36b597283c
gabrielgoes <gabrielgoes@usp.br>
2023-10-05 18:34:35 -0300

    condigo do baesa com tabela de informações gerais

--- boletim/machadinho/python/mapa_machadinho.py
+++ boletim/machadinho/python/mapa_machadinho.py
@@ -2,9 +2,10 @@ def generate_map_latex(output_filename):
     latex_code = r"""
 \begin{figure}[h]
     \centering
+	\captionsetup{justification=raggedright, singlelinecheck=false, width=1\textwidth}
     \caption{Mapa de eventos.}
-    \includegraphics[width=1.0\textwidth]{./figuras/mapaevents.png}
-    \caption*{Fonte:IPT}
+    \includegraphics[width=1.0\textwidth]{/home/ipt/projetos/ipt-latex/boletim/machadinho/figuras/mapaevents.png}
+    \caption*{Fonte: IPT}
 \end{figure}
 """
     with open(output_filename, "w") as output_file:
