6a7ae95d1ef4386fbf1617b5d58b62e9203c28c4
gabrielgoes <gabrielgoes@usp.br>
2023-10-06 14:31:13 -0300

    diretórios funcionam para qualquer máquina com o esquema de pastas ~/projetos/ipt-latex/[..]/

--- boletim/machadinho/python/tabela.py
+++ boletim/machadinho/python/tabela.py
@@ -2,10 +2,10 @@
 import csv
 
 # Nome do arquivo de entrada CSV
-input_csv = "/home/suporte/projetos/ipt-latex/boletim/machadinho/csv/events-2023-06-01-IT.csv"
+input_csv = "boletim/machadinho/csv/events-2023-06-01-IT.csv"
 
 # Nome do arquivo de saída LaTeX
-output_tex = "/home/suporte/projetos/ipt-latex/boletim/machadinho/tex/tabela_machadinho_boletim.tex"
+output_tex = "./boletim/machadinho/tex/tabela_machadinho_boletim.tex"
 
 # Abre o arquivo CSV para leitura
 with open(input_csv, "r") as csv_file:
